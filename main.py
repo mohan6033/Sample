21dcb8acb8b51e298f74b6977296894d9e4e7321
mohan6033 <141230133+mohan6033@users.noreply.github.com>
2023-12-29 09:42:00 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.markdown("This is my first App")
+st.write("Welcome to my Sample Streamlit App. This will bring a positive vibes to learn streamlit.It is a python Libary")
