fbec0a6da4503b8829ccbedfe005529ef0b1b06d
mohan6033 <141230133+mohan6033@users.noreply.github.com>
2023-12-29 14:38:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import streamlit as st
-st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
-#ing colour of test
-st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
-st.file_uploader("upload file")
+
 import streamlit as st
 import numpy as np
 
