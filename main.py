9b0ff9df9599da3ec009c13a5086c33de440cce3
mohan6033 <141230133+mohan6033@users.noreply.github.com>
2023-12-29 10:18:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import streamlit as st
-st.markdown("This is my first App")
-st.write("Welcome to my Sample Streamlit App. This will bring a positive vibes to learn streamlit.It is a python Libary")
+st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
+#ing colour of test
+st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
+st.file_uploader("upload file")
